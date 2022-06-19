0a4ec391a6f0753a5d7d4d2b3d819934044da243
Rafael <rldcarvalhoo@gmail.com>
2022-06-19 18:22:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto-jogo-da-memoria
+ Projeto de um jogo da memória com a temática de Pokemon criado com HTML, CSS e JS.
---
+++ assets/js/scripts.js
@@ -0,0 +1,66 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch()
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+/* Embaralha as Divs alterando a posição inicial de cada carta */
+(function shuffle() {
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 16);
+        card.style.order = randomPosition;
+    })
+})();
+
+
+
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard);
+})
+
